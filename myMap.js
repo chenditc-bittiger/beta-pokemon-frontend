8558dbb68feb36c696f413070e955e48664bdbca
chenditc-bittiger <chenditc.bittiger@gmail.com>
2016-12-03 22:25:39 -0500

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,6 +1,6 @@
 var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
     credentials: 'AjZmbGB9OLnThq1IWaOe8EqA1ivi2lExihwHHDoEG6jk8SrcTkLKHGKF4Pyr0biI'
 });
-var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://github.com/chenditc/mypokemon.io/raw/gh-pages/images/pushpin_images/pokemon/1.png',
+var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png',
     anchor: new Microsoft.Maps.Point(12, 39) });
 map.entities.push(pushpin);
