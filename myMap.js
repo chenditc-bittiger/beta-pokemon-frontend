e434d0fbf8d86920087bd80a2e09dbeb2d5421d8
chenditc-bittiger <chenditc.bittiger@gmail.com>
2016-12-03 22:33:56 -0500

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,10 +1,9 @@
-var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-    credentials: 'AjZmbGB9OLnThq1IWaOe8EqA1ivi2lExihwHHDoEG6jk8SrcTkLKHGKF4Pyr0biI'
-});
+function loadMapScenario() {
+    var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        credentials: 'AjZmbGB9OLnThq1IWaOe8EqA1ivi2lExihwHHDoEG6jk8SrcTkLKHGKF4Pyr0biI'
+    });
+    var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png',
+        anchor: new Microsoft.Maps.Point(12, 39) });
+    map.entities.push(pushpin);
 
-console.log("Before adding pushpin");
-var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png',
-    anchor: new Microsoft.Maps.Point(12, 39) });
-console.log("adding pushpin");
-map.entities.push(pushpin);
-console.log("Added pushpin");
+}
