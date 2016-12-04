32988f54d333c84d29c30df4f0b7df90e73f60f4
chenditc-bittiger <chenditc.bittiger@gmail.com>
2016-12-03 22:20:29 -0500

    Create myMap.js

---
+++ myMap.js
@@ -0,0 +1,6 @@
+var map;
+function loadMapScenario() {
+    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        credentials: 'AjZmbGB9OLnThq1IWaOe8EqA1ivi2lExihwHHDoEG6jk8SrcTkLKHGKF4Pyr0biI'
+    });
+}
