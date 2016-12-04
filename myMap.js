8afe8e42ffd5689e90cf9ba30100a64549c198c0
chenditc-bittiger <chenditc.bittiger@gmail.com>
2016-12-04 00:04:01 -0500

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -51,9 +51,8 @@ function loadMapScenario() {
         credentials: 'AjZmbGB9OLnThq1IWaOe8EqA1ivi2lExihwHHDoEG6jk8SrcTkLKHGKF4Pyr0biI'
     });
     map_manager.map = map;
-    query_pokemon_data();
+    window.setInterval(query_pokemon_data, 5000);
     window.setInterval(refresh_pokemon, 1000);
-
 }
 
 function refresh_pokemon() {
